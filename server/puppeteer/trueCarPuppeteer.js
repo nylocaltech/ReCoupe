906d4e22cd2c5e24c3148c2f101dc182eee7b5fd
Peng Dong <dongpeng101@gmail.com>
2022-06-16 00:27:27 -0400

    adding true car puppeteer

--- server/puppeteer/trueCarPuppeteer.js
+++ server/puppeteer/trueCarPuppeteer.js
@@ -1,9 +1,4 @@
 const puppeteer = require('puppeteer');
-const fs = require('fs/promises');
-const fsr = require('fs');
-let scrollToBottom = require("scroll-to-bottomjs");
-
-let photos    
 // "price" integer NOT NULL,
 // "image" varchar NOT NULL,
 // "mileage" integer NOT NULL,
@@ -20,111 +15,90 @@ async function start(make, model, minYear, zip){
   const page = await browser.newPage();
   //await page.goto(`https://www.truecar.com/used-cars-for-sale/listings/${make}/${model}/year-${minYear}-max/location-${zip}/?sort[]=price_asc`)
 
-  await page.goto(`https://www.truecar.com/used-cars-for-sale/listings/toyota/camry/year-2010-2012/location-11228/?sort[]=price_asc`)
+  await page.goto(`https://www.truecar.com/used-cars-for-sale/listings/toyota/camry/year-2012-max/location-11228/?sort[]=price_asc`)
 
+  let Alldata = [];
 
-  let data = [];
-  let prices;
+  //price
+  let priceData = await page.$$eval(".heading-3.margin-y-1.font-weight-bold", (data) => 
+  data.map((singleData) => parseInt(singleData.textContent.slice(1).split(',').join(''))
+  ))
+  console.log(priceData.length, 'price length')
 
-  const allData = await page.$$eval("#mainContent > div > div.container.container-max-width-3.deprecated-padding-1 > div > div > div > div.margin-bottom-2 > div:nth-child(1) > ul", (data) => {
-    return data.map(singleData => {
-      return singleData.textContent
-    })
+  //image
+  const imageData = await page.$$eval(".img-container.img-container-block", (data) => 
+  data.map((singleData) => singleData.querySelector('img').src)
+  )
+  let correctImage = []
+  if(imageData.length > 50){
+    for(let i = 0; i < imageData.length; i += 5){
+      correctImage.push(imageData[i]);
+    }
+  }
+  console.log(correctImage.length, 'imageData length');
+
+  //mileage
+  let mileageData = await page.$$eval(".font-size-1.text-truncate", (data) => 
+    data.map((singleData) => singleData.textContent)
+  )
+  let mileage = [];
+  for(let i = 2; i < mileageData.length; i += 4){
+    let removeSpace = mileageData[i].split(' ');
+    let removeCommaAndJoin = parseInt(removeSpace[0].split(',').join(''));
+    mileage.push(removeCommaAndJoin);
+  }
+  console.log(mileage.length, 'mileage length')
+
+  //year
+  const yearData = await page.$$eval(".vehicle-card-year.font-size-1", (data) => 
+    data.map((singleData) => parseInt(singleData.textContent))
+  )
+  console.log(yearData.length, 'year length')
+
+  //made and model
+  const makeData = await page.$$eval(".vehicle-header-make-model.text-truncate", (data) => 
+    data.map((singleData) => singleData.textContent)
+  )
+  const makeD = [], modelD = [];
+  makeData.forEach(element => {
+    let makeAndModel = element.split(' ');
+    makeD.push(makeAndModel[0]);
+    modelD.push(makeAndModel[1]);
   })
+  console.log(makeD.length, 'make length');
+  console.log(modelD.length, 'model length');
 
-  console.log(allData)
-  // const allPrices = await page.$$eval(".heading-3.margin-y-1.font-weight-bold", (prices) => 
-  //     prices.map((singlePrice) => singlePrice.textContent)
-  // )
-  // console.log(allPrices)
-  // let allPrice = allPrices[0];
+  //url
+  const urlData = await page.$$eval(".linkable.card.card-shadow.vehicle-card._1qd1muk"
+  , (data) => 
+  data.map((singleData) => singleData.querySelector('a').href)
+  )
+  console.log(urlData.length, 'url length');
 
-  // //split allcars data into an array
-   let allcarsdata = allData[0].split('Toyota')
-  // for(let i = 0; i < allcarsdata.length; i++){
-  //   allcarsdata[i] = allcarsdata[i] + '\n';
-  // }
+  //zip
+  console.log(11228, 'zip');
 
-  // console.log(allcarsdata)
-  await fs.writeFile('price.txt', allcarsdata);
+  //date
+  const date = new Date();
+  const actualDate = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`
+  console.log(actualDate, 'date')
 
-
-  //await page.screenshot({path: 'trueCar.png', fullPage: true})
-
-  // const photos = await page.$$eval(".card-image.vehicle-card-image div img[src]", (imgs) => {
-  //   console.log(imgs)
-  //   return imgs.map(x => x.getAttribute('src') + '\n')
-  // })
-
-  // await page.setRequestInterception(true);
-
-  // await page.evaluate(scrollToBottom);
-
-// div.container.container-max-width-3.deprecated-padding-1 > div > div > div > div.margin-bottom-2 > div:nth-child(1) > ul > li:nth-child(1) > div > div
-
-  // photos = await page.$$eval(".card-image.vehicle-card-image div img[src]", (imgs) => {
-  //   console.log(imgs)
-  //   const photosLinks = [];
-  //   const carPhotos = {};
-  //   imgs.map(x => {
-  //     let src = x.getAttribute('src');
-  //     let word = src.split('/')
-  //     if(!carPhotos[word[4]]){
-  //       carPhotos[word[4]] = src;
-  //       photosLinks.push(src);
-  //     }
-  //   })
-  //   return photosLinks; 
-  // })
-
-
-  // // function to check if new src link should be appended
-  // for (let newPhoto of photos) {
-  //   newPhoto = newPhoto + '\n';
-  //   let shouldAddtoFile = true;
-
-  //   //console.log(typeof fsr.readFileSync('img1.txt',{encoding:'utf8', flag:'r'}))
-  //   fsr.readFile('img1.txt', 'utf8', function(err, data){
-  //     // Display the file content
-  //     let arr = data.split('\n')
-  //     arr.forEach(async (oldData) => {
-  //       console.log(oldData, '<--oldData');
-  //       console.log(newPhoto, '<--newPhoto')
-  //       console.log(oldData.localeCompare(newPhoto.split('\n')[0]) != 0)
-  //     //only add when there is new data
-  //     if(oldData === newPhoto.split('\n')[0]){
-  //       shouldAddtoFile = false;
-  //     }
-  //   })
-  //   if (shouldAddtoFile){
-  //     fsr.appendFile('img1.txt', newPhoto, (err) => {
-  //       //console.log(err);
-  //     });
-  //   }
-  // });
-    
-  //   //if(fs.readFile())
-  // }
-
+  for(let i = 0; i < priceData.length; i++){
+    let data = {
+      price: priceData[i],
+      image: correctImage[i],
+      mileage: mileage[i],
+      year: yearData[i],
+      make: makeD[i],
+      model: modelD[i],
+      url: urlData[i],
+      zip: 11228,
+      date: actualDate,
+    }
+    Alldata.push(data)
+  }
+  console.log(Alldata)
   await browser.close()
-  // for (let photo of photos) console.log(photo, '<--photo');
 }
 
 start();
-
-
-  // let file = fsr.readFile('img1.txt', 'utf8', function(err, data){
-  // // Display the file content
-  //   console.log(data, 'data');
-  // });
-  // console.log(file)
-
-// console.log(typeof fsr.readFileSync('img1.txt',{encoding:'utf8', flag:'r'}))
-// let file = fsr.readFileSync('img1.txt',{encoding:'utf8', flag:'r'})
-// let arr = file.split('\n')
-
-//console.log(arr)
-
-//#mainContent > div > div.container.container-max-width-3.deprecated-padding-1 > div > div.col-lg-8.col-xl-9 > div > div.margin-bottom-2 > div:nth-child(1) > ul > li:nth-child(1) > div > div > div.carousel.w-100.position-relative > div > div > ul > li:nth-child(1) > a > div > div > img
-//card-image.vehicle-card-image > div > img
-//#mainContent > div > div.container.container-max-width-3.deprecated-padding-1 > div > div.col-lg-8.col-xl-9 > div > div.margin-bottom-2 > div:nth-child(1) > ul > li:nth-child(28) > div > div > div.card-image.vehicle-card-image > div
