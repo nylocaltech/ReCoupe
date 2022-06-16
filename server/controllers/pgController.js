f9701c9d560241ede01faee5cb360a9f2c974f6c
vongarcia97 <vongarcia97@gmail.com>
2022-06-16 12:08:43 -0400

    merging features SQL queries and backend routes

--- server/controllers/pgController.js
+++ server/controllers/pgController.js
@@ -1,73 +1,73 @@
-const db = require('../models/pgModels.js');
-const puppeteer = require('puppeteer');
+const db = require("../models/pgModels.js");
+const puppeteer = require("puppeteer");
 // carsDotComScraper = require('../scrapers/carsDotComScraper')
-const carsDotComScraper = require('../scrapers/carsDotComScraper.js')
-const autoTraderScraper = require('../scrapers/autoTraderScraper.js')
-const carGurusScraper = require('../scrapers/carGurusScraper.js');
-const trueCarScraper = require('../scrapers/trueCarScraper.js');
-
+const carsDotComScraper = require("../scrapers/carsDotComScraper.js");
+const autoTraderScraper = require("../scrapers/autoTraderScraper.js");
+const carGurusScraper = require("../scrapers/carGurusScraper.js");
+const trueCarScraper = require("../scrapers/trueCarScraper.js");
 
 const pgController = {};
 
 pgController.getCarsComData = async (req, res, next) => {
-  try{
+  try {
     //console.log(req.params)
     const { make, model, minYear, zip } = req.params;
     res.locals.carsComData = await carsDotComScraper(make, model, minYear, zip);
 
     return next();
+  } catch (err) {
+    return next({
+      log: "Error inside the getCarsComData middleware",
+      message: { err },
+    });
   }
-  catch(err){
-    return next({ 
-      log: 'Error inside the getCarsComData middleware',
-      message: { err }
-    })
-  }
-}
+};
 
 pgController.getAutoTraderData = async (req, res, next) => {
-  try{
-  const { make, model, minYear, zip } = req.params;
-  res.locals.autoTraderData = await autoTraderScraper(make, model, minYear, zip);
- 
-  return next();
-  }
-  catch(err){
-    return next({ 
-      log: 'Error inside the getCarsComData middleware',
-      message: { err }
-    })
-  }
- }
+  try {
+    const { make, model, minYear, zip } = req.params;
+    res.locals.autoTraderData = await autoTraderScraper(
+      make,
+      model,
+      minYear,
+      zip
+    );
 
- pgController.getCarGurusData = async (req, res, next) => {
+    return next();
+  } catch (err) {
+    return next({
+      log: "Error inside the getCarsComData middleware",
+      message: { err },
+    });
+  }
+};
+
+pgController.getCarGurusData = async (req, res, next) => {
   try {
     const { make, model, minYear, zip } = req.params;
     res.locals.carGurusData = await carGurusScraper(make, model, minYear, zip);
     return next();
+  } catch (err) {
+    return next({
+      log: "Error inside the getCarGurusData middleware",
+      message: { err },
+    });
   }
-  catch(err){
-    return next({ 
-      log: 'Error inside the getCarGurusData middleware',
-      message: { err }
-    })
-  }
- }
+};
 
- pgController.getTrueCarData = async (req, res, next) => {
-  try{
+pgController.getTrueCarData = async (req, res, next) => {
+  try {
     const { make, model, minYear, zip } = req.params;
     res.locals.trueCarData = await trueCarScraper(make, model, minYear, zip);
-   
+
     return next();
+  } catch (err) {
+    return next({
+      log: "Error inside the getTrueCarData middleware",
+      message: { err },
+    });
   }
-  catch(err){
-    return next({ 
-      log: 'Error inside the getTrueCarData middleware',
-      message: { err }
-    })
-  }
-}
+};
 
 // pgController.getCarGuruData = (req, res, next) => {
 //   const { make, model, minYear, zip } = req.body;
@@ -76,8 +76,8 @@ pgController.getAutoTraderData = async (req, res, next) => {
 //  }
 
 // APi (/asdasd/, Carsdotcomcontroller, cargurucontroller, => {
-//   res.locals.carguru, res.locals.carsdotcome 
-  
+//   res.locals.carguru, res.locals.carsdotcome
+
 // })
 
 pgController.insertCarsComData = async (req, res, next) => {
@@ -98,50 +98,54 @@ pgController.insertCarsComData = async (req, res, next) => {
 
   //------------------- CONCAT version below
 
-  let queryStr = `INSERT INTO cars(price, image, mileage, year, make, model, url, zip, date) VALUES`                 
-  carsComData.forEach(car => {
-    console.log('hello we are in the loop pls help')
+  let queryStr = `INSERT INTO cars(price, image, mileage, year, make, model, url, zip, date) VALUES`;
+  carsComData.forEach((car) => {
+    console.log("hello we are in the loop pls help");
     const { price, image, mileage, year, make, model, url, zip, date } = car;
-    
-    queryStr += `(${price}, ${image}, ${mileage}, ${year}, ${make}, ${model}, ${url}, ${zip}, ${date}),`
-  })
+
+    queryStr += `(${price}, ${image}, ${mileage}, ${year}, ${make}, ${model}, ${url}, ${zip}, ${date}),`;
+  });
   queryStr = queryStr.slice(0, -1);
-  queryStr += ';'
+  queryStr += ";";
   //queryStr = queryStr.replace(/.$/, ';');
-  console.log('last char is:', queryStr[queryStr.length-1]);
+  console.log("last char is:", queryStr[queryStr.length - 1]);
 
-  await db.query(queryStr)
-      .then(res => {
-        console.log(res.rows)
-        return next()
+  await db
+    .query(queryStr)
+    .then((res) => {
+      console.log(res.rows);
+      return next();
+    })
+    .catch((err) =>
+      next({
+        log: "Error inside the insertCarsComData middleware",
+        message: { err },
       })
-      .catch(err => next({ 
-        log: 'Error inside the insertCarsComData middleware',
-        message: { err }
-      }));
-}
-
+    );
+};
 
 // add controllers
 pgController.getSavedData = (req, res, next) => {
-  const {make, model} = req.params
+  const { make, model } = req.params;
   // const values = [make,model];
   const queryStr = `SELECT c.make AS make, c.model, p.year, p.date, p.price, p.url
   FROM prices AS p
   JOIN cars AS c
   ON p.car_id = c._id
-  WHERE c.make = "Honda";`
+  WHERE c.make = "Honda";`;
   //I'm having problems with the query string, if you get rid of everything after the 'WHERE' it will return all two tables mixed, but if I specify what I just want the make Honda it crashes
 
-   db.query(queryStr)
-    .then(carInfo => {
-      console.log(carInfo.rows)
+  db.query(queryStr)
+    .then((carInfo) => {
+      console.log(carInfo.rows);
       return next();
     })
-    .catch(err => next({ 
-      log: 'Error inside the getSavedData middleware',
-      message: { err }
-    }));
-}
+    .catch((err) =>
+      next({
+        log: "Error inside the getSavedData middleware",
+        message: { err },
+      })
+    );
+};
 
 module.exports = pgController;
